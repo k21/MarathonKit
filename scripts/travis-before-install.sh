cdcab8bec989b4c4a81750f631fe65760cbc8e56
Jakub Zika <j@kub-zika.cz>
2016-04-14 00:49:03 +0100

    Run Travis builds with multiple compiler versions

--- scripts/travis-before-install.sh
+++ scripts/travis-before-install.sh
@@ -1,11 +1,57 @@
-#!/bin/sh
+#!/bin/bash
 set -eux
 
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+add_llvm_repository() {
+  local VERSION="$1"
+  local URL="http://llvm.org/apt/trusty/"
+  local DIST="llvm-toolchain-trusty-${VERSION}"
+
+  # Downloading a key over insecure connection...
+  # Do not do this on any machine you like.
+  wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
+
+  sudo add-apt-repository "deb ${URL} ${DIST} main"
+}
+
+escape() {
+  sed "s/'/'\\\\''/g;s/.*/'&'/"
+}
+
+install_tool() {
+  local SOURCE="$1"
+  local TARGET="$2"
+  {
+    echo "#!/bin/sh"
+    echo "exec $(echo "${SOURCE}" | escape) "'"$@"'
+  } >"${TARGET}"
+  chmod +x "${TARGET}"
+  cat "${TARGET}"
+}
+
+mkdir -p ~/bin
+
+if [ "x${CXX}" = "xg++" ]
+then
+  sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+elif [ "x${CXX}" = "xclang++" ]
+then
+  add_llvm_repository "${CLANG_VERSION}"
+else
+  echo "Unknown CXX: ${CXX}" >&2
+  exit 1
+fi
+
 sudo apt-get update -qq
 sudo apt-get install -qq libsndfile-dev
-if [ "x$CXX" = "xg++" ]
+
+if [ "x${CXX}" = "xg++" ]
 then
-  sudo apt-get install -qq g++-4.8
-  sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
+  sudo apt-get install -qq "g++-${GCC_VERSION}"
+  install_tool "/usr/bin/gcc-${GCC_VERSION}" ~/bin/cc
+  install_tool "/usr/bin/g++-${GCC_VERSION}" ~/bin/cxx
+elif [ "x${CXX}" = "xclang++" ]
+then
+  sudo apt-get install -qq "clang-${CLANG_VERSION}"
+  install_tool "/usr/bin/clang-${CLANG_VERSION}" ~/bin/cc
+  install_tool "/usr/bin/clang++-${CLANG_VERSION}" ~/bin/cxx
 fi
