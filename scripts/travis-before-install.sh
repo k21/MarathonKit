6a235d2ac204c550f24061d80a1f69dfe1930b9e
Jakub Zika <j@kub-zika.cz>
2016-04-15 17:04:59 +0100

    Run Travis builds on OSX in addition to Linux

--- scripts/travis-before-install.sh
+++ scripts/travis-before-install.sh
@@ -28,30 +28,60 @@ install_tool() {
   cat "${TARGET}"
 }
 
+setup_linux() {
+  if [ "x${CXX}" = "xg++" ]
+  then
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+  elif [ "x${CXX}" = "xclang++" ]
+  then
+    add_llvm_repository "${CLANG_VERSION}"
+  else
+    echo "Unknown CXX: ${CXX}" >&2
+    exit 1
+  fi
+
+  sudo apt-get update -qq
+  sudo apt-get install -qq libsndfile-dev
+
+  if [ "x${CXX}" = "xg++" ]
+  then
+    sudo apt-get install -qq "g++-${GCC_VERSION}"
+    install_tool "/usr/bin/gcc-${GCC_VERSION}" ~/bin/cc
+    install_tool "/usr/bin/g++-${GCC_VERSION}" ~/bin/cxx
+  elif [ "x${CXX}" = "xclang++" ]
+  then
+    sudo apt-get install -qq "clang-${CLANG_VERSION}"
+    install_tool "/usr/bin/clang-${CLANG_VERSION}" ~/bin/cc
+    install_tool "/usr/bin/clang++-${CLANG_VERSION}" ~/bin/cxx
+  fi
+}
+
+setup_osx() {
+  brew update
+  brew install libsndfile
+  if [ "x${CXX}" = "xg++" ]
+  then
+    install_tool "$(which gcc)" ~/bin/cc
+    install_tool "$(which g++)" ~/bin/cxx
+  elif [ "x${CXX}" = "xclang++" ]
+  then
+    install_tool "$(which clang)" ~/bin/cc
+    install_tool "$(which clang++)" ~/bin/cxx
+  else
+    echo "Unknown CXX: ${CXX}" >&2
+    exit 1
+  fi
+}
+
 mkdir -p ~/bin
 
-if [ "x${CXX}" = "xg++" ]
+if [ "x${TRAVIS_OS_NAME}" = 'xosx' ]
 then
-  sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-elif [ "x${CXX}" = "xclang++" ]
+  setup_osx
+elif [ "x${TRAVIS_OS_NAME}" = 'xlinux' ]
 then
-  add_llvm_repository "${CLANG_VERSION}"
+  setup_linux
 else
-  echo "Unknown CXX: ${CXX}" >&2
+  echo "Unknown TRAVIS_OS_NAME: ${TRAVIS_OS_NAME}" >&2
   exit 1
-fi
-
-sudo apt-get update -qq
-sudo apt-get install -qq libsndfile-dev
-
-if [ "x${CXX}" = "xg++" ]
-then
-  sudo apt-get install -qq "g++-${GCC_VERSION}"
-  install_tool "/usr/bin/gcc-${GCC_VERSION}" ~/bin/cc
-  install_tool "/usr/bin/g++-${GCC_VERSION}" ~/bin/cxx
-elif [ "x${CXX}" = "xclang++" ]
-then
-  sudo apt-get install -qq "clang-${CLANG_VERSION}"
-  install_tool "/usr/bin/clang-${CLANG_VERSION}" ~/bin/cc
-  install_tool "/usr/bin/clang++-${CLANG_VERSION}" ~/bin/cxx
 fi
